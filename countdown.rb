015d06a509aeeb3461f19dbb5da4169285917d70
Sarah Keller <sebethkeller@gmail.com>
2016-08-01 19:03:45 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  for i in 0...n
+    puts "#{n-i} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  until n == 0
+    puts "#{n} SECOND(S)!"
+    n-=1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
